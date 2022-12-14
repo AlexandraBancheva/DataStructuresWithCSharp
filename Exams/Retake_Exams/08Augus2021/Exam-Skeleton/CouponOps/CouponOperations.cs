04ba7eb4c7ec0794ada3ff0b8254a70140e812a1
AlexandraBancheva <sandriche90@gmail.com>
2022-12-14 18:47:51 +0200

    Added retake exam - 08August2021

---
+++ Exams/Retake_Exams/08Augus2021/Exam-Skeleton/CouponOps/CouponOperations.cs
@@ -0,0 +1,120 @@
+﻿namespace CouponOps
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using CouponOps.Models;
+    using Interfaces;
+
+    public class CouponOperations : ICouponOperations
+    {
+        private Dictionary<string, Website> websitesByName = new Dictionary<string, Website>();
+        private Dictionary<string, Coupon> couponsByCode = new Dictionary<string, Coupon>();
+
+        public void AddCoupon(Website website, Coupon coupon)
+        {
+            if (!websitesByName.ContainsKey(website.Domain))
+            {
+                throw new ArgumentException();
+            }
+
+            coupon.Website = website;
+            couponsByCode.Add(coupon.Code, coupon);
+            websitesByName[website.Domain].Coupons.Add(coupon);
+        }
+
+        public bool Exist(Website website)
+        {
+            return websitesByName.ContainsKey(website.Domain) ? true : false;
+        }
+
+        public bool Exist(Coupon coupon)
+        {
+            return couponsByCode.ContainsKey(coupon.Code) ? true : false;
+        }
+
+        public IEnumerable<Coupon> GetCouponsForWebsite(Website website)
+        {
+            if (!websitesByName.ContainsKey(website.Domain))
+            {
+                throw new ArgumentException();
+            }
+            return couponsByCode.Values.Where(w => w.Website.Domain == website.Domain);
+        }
+
+        public IEnumerable<Coupon> GetCouponsOrderedByValidityDescAndDiscountPercentageDesc()
+        {
+            return couponsByCode.Values
+                .OrderByDescending(c => c.Validity)
+                .ThenByDescending(c => c.DiscountPercentage);
+        }
+
+        public IEnumerable<Website> GetSites()
+        {
+            return websitesByName.Values;
+        }
+
+        public IEnumerable<Website> GetWebsitesOrderedByUserCountAndCouponsCountDesc()
+        {
+            var res =  websitesByName.Values
+                .OrderBy(w => w.UsersCount)
+                .ThenByDescending(w => w.Coupons.Count).ToList();
+
+            return res;
+        }
+
+        public void RegisterSite(Website website)
+        {
+            if (websitesByName.ContainsKey(website.Domain))
+            {
+                throw new ArgumentException();
+            }
+
+            websitesByName.Add(website.Domain, website);
+        }
+
+        public Coupon RemoveCoupon(string code)
+        {
+            if (!couponsByCode.ContainsKey(code))
+            {
+                throw new ArgumentException();
+            }
+            var deletedCoupon = couponsByCode[code];
+            couponsByCode.Remove(code);
+
+            return deletedCoupon;
+        }
+
+        public Website RemoveWebsite(string domain)
+        {
+            if (!websitesByName.ContainsKey(domain))
+            {
+                throw new ArgumentException();
+            }
+            var deletedWebsite = websitesByName[domain];
+            websitesByName.Remove(domain);
+            foreach (var coupon in deletedWebsite.Coupons)
+            {
+                couponsByCode.Remove(coupon.Code);
+            }
+
+            return deletedWebsite;
+        }
+
+        public void UseCoupon(Website website, Coupon coupon)
+        {
+            if (!Exist(website) || !Exist(coupon))
+            {
+                throw new ArgumentException();
+            }
+
+            if (!website.Coupons.Contains(coupon))
+            {
+                throw new ArgumentException();
+            }
+
+            RemoveCoupon(coupon.Code);
+        }
+    }
+}
---
+++ Exams/Retake_Exams/08Augus2021/Exam-Skeleton/VaccOps/VaccDb.cs
@@ -0,0 +1,110 @@
+﻿namespace VaccOps
+{
+    using Models;
+    using Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class VaccDb : IVaccOps
+    {
+        private Dictionary<string, Doctor> doctorsByName = new Dictionary<string, Doctor>();
+        private Dictionary<string, Patient> patientsByName = new Dictionary<string, Patient>();
+
+        public void AddDoctor(Doctor doctor)
+        {
+            if (this.doctorsByName.ContainsKey(doctor.Name))
+            {
+                throw new ArgumentException();
+            }
+
+            this.doctorsByName.Add(doctor.Name, doctor);
+        }
+
+        public void AddPatient(Doctor doctor, Patient patient)
+        {
+            if (!this.doctorsByName.ContainsKey(doctor.Name))
+            {
+                throw new ArgumentException();
+            }
+            this.patientsByName.Add(patient.Name, patient);
+            this.doctorsByName[doctor.Name].Patients.Add(patient);
+            patient.Doctor = doctor;
+        }
+
+        public void ChangeDoctor(Doctor oldDoctor, Doctor newDoctor, Patient patient)
+        {
+            if (!this.doctorsByName.ContainsKey(oldDoctor.Name) || !this.doctorsByName.ContainsKey(newDoctor.Name)
+                || !this.patientsByName.ContainsKey(patient.Name))
+            {
+                throw new ArgumentException();
+            }
+            oldDoctor.Patients.Remove(patient);
+            newDoctor.Patients.Add(patient);
+            patient.Doctor = newDoctor;
+        }
+
+        public bool Exist(Doctor doctor)
+        {
+            return this.doctorsByName.ContainsKey(doctor.Name);
+        }
+
+        public bool Exist(Patient patient)
+        {
+            return this.patientsByName.ContainsKey(patient.Name);
+        }
+
+        public IEnumerable<Doctor> GetDoctors()
+        {
+            return this.doctorsByName.Values;
+        }
+
+        public IEnumerable<Doctor> GetDoctorsByPopularity(int populariry)
+        {
+           return this.doctorsByName.Values.Where(doc => doc.Popularity == populariry).ToList();
+        }
+
+        public IEnumerable<Doctor> GetDoctorsSortedByPatientsCountDescAndNameAsc()
+        {
+            return this.GetDoctors().OrderByDescending(doc => doc.Patients.Count).ThenBy(doc => doc.Name);
+        }
+
+        public IEnumerable<Patient> GetPatients()
+        {
+            return this.patientsByName.Values;
+        }
+
+        public IEnumerable<Patient> GetPatientsByTown(string town)
+        {
+            return this.patientsByName.Values.Where(t => t.Town == town).ToList();
+        }
+
+        public IEnumerable<Patient> GetPatientsInAgeRange(int lo, int hi)
+        {
+            return this.patientsByName.Values.Where(pat => pat.Age <= hi && pat.Age >= lo);
+        }
+
+        public IEnumerable<Patient> GetPatientsSortedByDoctorsPopularityAscThenByHeightDescThenByAge()
+        {
+            return this.patientsByName.Values.OrderBy(pat => pat.Doctor.Popularity)
+                .ThenByDescending(pat => pat.Height)
+                .ThenBy(pat => pat.Age);
+        }
+
+        public Doctor RemoveDoctor(string name)
+        {
+            if (!this.doctorsByName.ContainsKey(name))
+            {
+                throw new ArgumentException();
+            }
+            var doctor = this.doctorsByName[name];
+            this.doctorsByName.Remove(name);
+            foreach (var patient in doctor.Patients)
+            {
+                this.patientsByName.Remove(patient.Name);
+            }
+
+            return doctor;
+        }
+    }
+}
