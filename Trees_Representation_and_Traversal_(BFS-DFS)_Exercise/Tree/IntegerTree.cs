71f9e4cf593ac8b6facc2eb8d6f75717f7156213
AlexandraBancheva <sandriche90@gmail.com>
2022-12-14 18:18:09 +0200

    Trees_Representation_and_Traversal_Exercises

---
+++ Trees_Representation_and_Traversal_(BFS-DFS)_Exercise/Tree/IntegerTree.cs
@@ -0,0 +1,102 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class IntegerTree : Tree<int>, IIntegerTree
+    {
+        public IntegerTree(int key, params Tree<int>[] children)
+            : base(key, children)
+        {
+        }
+
+        // DFS
+        public IEnumerable<IEnumerable<int>> GetPathsWithGivenSum(int sum)
+        {
+            var result = new List<List<int>>();
+            var currentPath = new LinkedList<int>();
+            currentPath.AddFirst(this.Key);
+
+            int currentSum = this.Key;
+            this.Dfs(this, result, currentPath, ref currentSum, sum);
+            return result;
+        }
+
+        private void Dfs(Tree<int> subtree,
+                         List<List<int>> result, 
+                         LinkedList<int> currentPath, 
+                         ref int currentSum, 
+                         int wantedSum)
+        {
+            foreach (var child in subtree.children)
+            {
+                currentSum += child.Key;
+                currentPath.AddLast(child.Key);
+                this.Dfs(child, result, currentPath, ref currentSum, wantedSum);
+            }
+
+            if (currentSum == wantedSum)
+            {
+                result.Add(new List<int>(currentPath));
+            }
+
+            currentSum -= subtree.Key;
+            currentPath.RemoveLast();
+        }
+
+        // BFS
+        public IEnumerable<Tree<int>> GetSubtreesWithGivenSum(int sum)
+        {
+            var result = new List<Tree<int>>();
+            var allSubtrees = this.GetAllNodesByBfs();
+
+            foreach (var subtree in allSubtrees)
+            {
+                if (this.HasGivenSum(subtree, sum))
+                {
+                    result.Add(subtree);
+                }
+            }
+
+            return result;
+        }
+
+        private bool HasGivenSum(Tree<int> subtree, int wantedSum)
+        {
+            var actualSum = subtree.Key;
+            this.GetSubtreeSumByDfs(subtree, wantedSum, ref actualSum);
+
+            return actualSum == wantedSum;
+        }
+
+        private void GetSubtreeSumByDfs(Tree<int> subtree, int wantedSum, ref int actualSum)
+        {
+            foreach (var child in subtree.Children)
+            {
+                actualSum += child.Key;
+                this.GetSubtreeSumByDfs(child, wantedSum, ref actualSum);
+            }
+        }
+
+        private List<Tree<int>> GetAllNodesByBfs()
+        {
+            var result = new List<Tree<int>>();
+            var queue = new Queue<Tree<int>>();
+
+            queue.Enqueue(this);
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                result.Add(current);
+
+                foreach (var child in current.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Trees_Representation_and_Traversal_(BFS-DFS)_Exercise/Tree/Tree.cs
@@ -0,0 +1,147 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        public List<Tree<T>> children { get; set; }
+
+        public Tree(T key, params Tree<T>[] children)
+        {
+            this.Key = key;
+            this.children = new List<Tree<T>>();
+
+            foreach (var child in children)
+            {
+                this.AddChild(child);
+                child.Parent = this;
+            }
+        }
+
+        public T Key { get; private set; }
+
+        public Tree<T> Parent { get; private set; }
+
+        public IReadOnlyCollection<Tree<T>> Children => this.children.AsReadOnly();
+
+        public void AddChild(Tree<T> child)
+        {
+            this.children.Add(child);
+        }
+
+        public void AddParent(Tree<T> parent)
+        {
+            this.Parent = parent;
+        }
+
+        public string AsString()
+        {
+            var sb = new StringBuilder();
+
+            this.DfsAsString(sb, this, 0);
+
+            return sb.ToString().Trim();
+        }
+
+        private void DfsAsString(StringBuilder sb, Tree<T> tree, int indent)
+        {
+            sb.Append(' ', indent)
+                .AppendLine(tree.Key.ToString());
+
+            foreach (var child in tree.children)
+            {
+                this.DfsAsString(sb, child, indent + 2);
+            }
+        }
+
+        public IEnumerable<T> GetInternalKeys()
+        {
+            return this.BfsWithResultKeys(tree => tree.children.Count > 0 && tree.Parent != null)
+                .Select(tree => tree.Key);
+        }
+
+        public IEnumerable<T> GetLeafKeys()
+        {
+            return this.BfsWithResultKeys(tree => tree.children.Count == 0)
+                .Select(tree => tree.Key);
+        }
+
+        private IEnumerable<Tree<T>> BfsWithResultKeys(Predicate<Tree<T>> predicate)
+        {
+            var result = new List<Tree<T>>();
+            var queue = new Queue<Tree<T>>();
+            queue.Enqueue(this);
+
+            while (queue.Count > 0)
+            {
+                var currentSubtree = queue.Dequeue();
+                if (predicate.Invoke(currentSubtree))
+                {
+                    result.Add(currentSubtree);
+                }
+
+                foreach (var child in currentSubtree.children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return result;
+        }
+
+        public T GetDeepestKey()
+        {
+            return this.GetDeepestNode().Key;
+        }
+
+        private Tree<T> GetDeepestNode()
+        {
+            var leafs = this.BfsWithResultKeys(tr => tr.children.Count == 0);
+
+            Tree<T> deepestNode = null;
+            var maxDepth = 0;
+
+            foreach (var leaf in leafs)
+            {
+                var depth = this.GetDepth(leaf);
+                if (depth > maxDepth)
+                {
+                    maxDepth = depth;
+                    deepestNode = leaf;
+                }
+            }
+
+            return deepestNode;
+        }
+
+        private int GetDepth(Tree<T> leaf)
+        {
+            int depth = 0;
+            var tree = leaf;
+            while (tree.Parent != null)
+            {
+                depth++;
+                tree = tree.Parent;
+            }
+
+            return depth;
+        }
+
+        public IEnumerable<T> GetLongestPath()
+        {
+            var current = this.GetDeepestNode();
+            var longestPath = new Stack<T>();
+
+            while (current != null)
+            {
+                longestPath.Push(current.Key);
+                current = current.Parent;
+            }
+
+            return longestPath;
+        }
+    }
+}
