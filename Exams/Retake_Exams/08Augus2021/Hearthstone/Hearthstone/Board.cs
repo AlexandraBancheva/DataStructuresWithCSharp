a3114863c707838f80e48659294ee0362f65797a
AlexandraBancheva <sandriche90@gmail.com>
2022-12-14 18:52:11 +0200

    Added exam - 19March2022

---
+++ Exams/Retake_Exams/08Augus2021/Hearthstone/Hearthstone/Board.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Board : IBoard
+{
+    private Dictionary<string, Card> deck = new Dictionary<string, Card>();
+
+    public bool Contains(string name)
+    {
+        return deck.ContainsKey(name);
+    }
+
+    public int Count()
+    {
+        return deck.Count;
+    }
+
+    public void Draw(Card card)
+    {
+        if (deck.ContainsKey(card.Name))
+        {
+            throw new ArgumentException();
+        }
+
+        deck.Add(card.Name, card);
+    }
+
+    public IEnumerable<Card> GetBestInRange(int start, int end)
+    {
+        return deck.Values.Where(card => card.Score >= start && card.Score <= end)
+            .OrderByDescending(card => card.Level);
+    }
+
+    public void Heal(int health)
+    {
+        deck.Values.OrderBy(c => c.Health).FirstOrDefault().Health += health;
+    }
+
+    public IEnumerable<Card> ListCardsByPrefix(string prefix)
+    {
+        return deck.Values
+            .Where(c => c.Name.StartsWith(prefix))
+            .OrderBy(c => string.Join("", c.Name.Reverse()))
+            .ThenBy(c => c.Level);
+    }
+
+    public void Play(string attackerCardName, string attackedCardName)
+    {
+        if (!deck.ContainsKey(attackerCardName) || !deck.ContainsKey(attackedCardName))
+        {
+            throw new ArgumentException();
+        }
+
+        var attacker = deck[attackerCardName];
+        var defender = deck[attackedCardName];
+
+        if (attacker.Level != defender.Level)
+        {
+            throw new ArgumentException();
+        }
+
+        if (defender.Health <= 0 || attacker.Health <= 0)
+        {
+            return;
+        }
+
+        defender.Health -= attacker.Damage;
+
+        if (defender.Health <= 0)
+        {
+            attacker.Score += defender.Level;
+        }
+    }
+
+    public void Remove(string name)
+    {
+        if (!deck.ContainsKey(name))
+        {
+            throw new ArgumentException();
+        }
+
+        deck.Remove(name);
+    }
+
+    public void RemoveDeath()
+    {
+        deck = deck
+                .Where(kvp => kvp.Value.Health > 0)
+                .ToDictionary(k => k.Key, v => v.Value);
+    }
+
+    public IEnumerable<Card> SearchByLevel(int level)
+    {
+        return deck.Values.Where(card => card.Level == level).OrderByDescending(card => card.Score);
+    }
+}
---
+++ Exams/Retake_Exams/08Augus2021/Olympics/Olympics/Olympics.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Olympics : IOlympics
+{
+    private Dictionary<int, Competitor> competitors = new Dictionary<int, Competitor>();
+    private Dictionary<int, Competition> competitions = new Dictionary<int, Competition>();
+
+    public void AddCompetition(int id, string name, int participantsLimit)
+    {
+        if (this.competitions.ContainsKey(id))
+        {
+            throw new ArgumentException();
+        }
+        var competition = new Competition(name, id, participantsLimit);
+        this.competitions.Add(id,competition);
+    }
+
+    public void AddCompetitor(int id, string name)
+    {
+        if (this.competitors.ContainsKey(id))
+        {
+            throw new ArgumentException();
+        }
+        this.competitors.Add(id, new Competitor(id, name));
+    }
+
+    public void Compete(int competitorId, int competitionId)
+    {
+        if (!this.competitors.ContainsKey(competitorId) || !this.competitions.ContainsKey(competitionId))
+        {
+            throw new ArgumentException();
+        }
+        var competitor = this.competitors[competitorId];
+
+        competitor.TotalScore += this.competitions[competitionId].Score;
+        this.competitions[competitionId].Competitors.Add(competitor);
+    }
+
+    public int CompetitionsCount()
+    {
+        return this.competitions.Count;
+    }
+
+    public int CompetitorsCount()
+    {
+        return this.competitors.Count;
+    }
+
+    // Performance tests
+    public bool Contains(int competitionId, Competitor comp)
+    {
+        if (!this.competitions.ContainsKey(competitionId))
+        {
+            throw new ArgumentException();
+        }
+        
+        var res = this.competitions[competitionId].Competitors.Where(c => c.Id == comp.Id);
+
+        if (res != null)
+        {
+           return true;
+        }
+
+        return false;
+    }
+
+    public void Disqualify(int competitionId, int competitorId)
+    {
+        if (!this.competitors.ContainsKey(competitorId) || !this.competitions.ContainsKey(competitionId))
+        {
+            throw new ArgumentException();
+        }
+
+        var competition = this.competitions[competitionId];
+        var competitor = this.competitors[competitorId];
+
+        competitor.TotalScore -= competition.Score;
+        competition.Competitors.Remove(competitor);
+    }
+
+    public IEnumerable<Competitor> FindCompetitorsInRange(long min, long max)
+    {
+        var res = this.competitors.Values
+            .Where(c => c.TotalScore > min && c.TotalScore <= max)
+            .OrderBy(c => c.Id)
+            .ToList();
+        return res;
+    }
+
+    public IEnumerable<Competitor> GetByName(string name)
+    {
+        var competitor = this.competitors.Values.Where(c => c.Name == name);
+        if (competitor.Count() == 0)
+        {
+            throw new ArgumentException();
+        }
+
+        return this.competitors.Values.Where(c => c.Name == name).OrderBy(c => c.Id);
+    }
+
+    public Competition GetCompetition(int id)
+    {
+        if (!this.competitions.ContainsKey(id))
+        {
+            throw new ArgumentException();
+        }
+        return this.competitions[id];
+    }
+
+    public IEnumerable<Competitor> SearchWithNameLength(int min, int max)
+    {
+        return this.competitors.Values
+                .Where(c => c.Name.Length >= min && c.Name.Length <= max)
+                .OrderBy(c => c.Id);
+    }
+}
